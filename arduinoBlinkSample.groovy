296f385628432bc4ac19d1abba85a2d4a3245e9f
madhephaestus <mad.hephaestus@gmail.com>
2016-10-03 20:10:26 -0400

    remoing whitespace

--- arduinoBlinkSample.groovy
+++ arduinoBlinkSample.groovy
@@ -1,5 +1,3 @@
-//Your code here
-
 ScriptingEngine.gitScriptRun(
             "https://github.com/madhephaestus/ArduinoExamples.git", // git location of the library
             "01.Basics/Blink/Blink.ino" , // Arduino blink light demo
